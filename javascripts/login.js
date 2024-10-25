7828cf67cadfeae7cbd965ade183e8d530f63dec
Focal-textil <focalti@gmail.com>
2024-10-25 16:18:49 -0300

    Update login.js

--- javascripts/login.js
+++ javascripts/login.js
@@ -1,19 +1,20 @@
 // --------------------------SALVAR LOGIN NO LOCAL STORAGE-----------------
-// window.addEventListener('load', function () {
-//     const login_result = localStorage.getItem("login");
-    
-//     if(login_result === 1){
-        
-//         const tela = document.getElementById('login-tela');
-//         tela.classList.add('sumir');
-//         link.src = data.dashComercial;
-//     }
-    
 
-   
-// });
+window.addEventListener('load', function () {
+    let login_result = localStorage.getItem("login");
+    let tela = document.getElementById('login-tela');
+    
+    if(login_result === "1"){
+        tela.classList.add('sumir');
+
+        }else{
+            login_result = 0;
+        }
+        
+    });
 
 // --------------------------LOGICA PARA LOGAR NA PLATAFORMA-----------------
+
 document.getElementById('loginForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
@@ -29,14 +30,10 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
 
             if (username === validUsername && password === validPassword) {
 
-                // Salvar login no localStorage
-                localStorage.setItem('username', username);
-                localStorage.setItem('password', password);
-
                 const tela = document.getElementById('login-tela');
                 tela.classList.add('sumir');
                 link.src = data.dashComercial;
-                let logado = localStorage.setItem("login",1);
+                let logado = localStorage.setItem("login","1");
     
             } else {
                 document.getElementById('message').innerText = "Usuário ou senha incorretos!";
@@ -44,7 +41,7 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
         });
 
 
-    // ------------------------LOGICA DOS BOTOES-----------------
+    // ------------------------LOGICA DOS BOTOES---------------------------------
 
     const btnrep = document.getElementById('btn-rep');
 
@@ -84,5 +81,4 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
 });
 
 
-
-// --------------------------FIM-----------------
+// --------------------------FIM--------------------------------------------------------------
