e817ce60dd54e1a6c432dc40005900fe0397a351
Focal-textil <focalti@gmail.com>
2025-01-22 15:41:22 -0300

    updated

--- javascripts/login.js
+++ javascripts/login.js
@@ -1,3 +1,60 @@
+// document.getElementById('btn_entrar').addEventListener('click', function (event) {
+//     event.preventDefault();
+
+//     const username = document.getElementById('username').value.trim();
+//     const password = document.getElementById('password').value.trim();
+//     const message = document.getElementById('message');
+
+//     if (!username || !password) {
+//         message.innerText = "Preencha todos os campos.";
+//         message.style.color = "red";
+//         return;
+//     }
+
+//     fetch('crendencials/keys.json')
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Erro ao acessar o arquivo de credenciais.');
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             const user = data.find(item => item.user === username && item.password === password);
+
+//             if (user) {
+//                 localStorage.setItem("logon", 1);
+//                 localStorage.setItem("usuario", user.user);
+//                 window.location.replace(user.tela);
+//             } else {
+//                 message.innerText = "Usuário ou senha incorretos!";
+//                 message.style.color = "red";
+//             }
+//         })
+//         .catch(error => {
+//             console.error('Erro ao carregar as credenciais:', error);
+//             message.innerText = "Erro ao carregar dados de autenticação.";
+//             message.style.color = "red";
+//         });
+// });
+
+
+// function Logado() {
+//     document.addEventListener("click", function (event) {
+    
+//         if (event.target && event.target.classList.contains("exit")) {
+//             localStorage.clear();
+//             localStorage.setItem("logon", 0);
+//             window.location.replace("index.html");
+//         }
+//     });
+// }
+
+
+// window.onload = function () {
+//     Logado();
+// };
+
+
 document.getElementById('btn_entrar').addEventListener('click', function (event) {
     event.preventDefault();
 
@@ -11,22 +68,36 @@ document.getElementById('btn_entrar').addEventListener('click', function (event)
         return;
     }
 
-    fetch('crendencials/keys.json')
+  
+    const googleSheetsUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSU6o-0moYLxGrlKiKFjFav-PFmUKG79CCL4WTOT7q7pQ8_LFqtDGf3uoKhgHqVORc0a8yJrbjThAM2/pub?gid=0&single=true&output=csv';
+
+    fetch(googleSheetsUrl)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Erro ao acessar o arquivo de credenciais.');
             }
-            return response.json();
+            return response.text();
         })
-        .then(data => {
-            const user = data.find(item => item.user === username && item.password === password);
+        .then(csvData => {
+    
+            const rows = csvData.split('\n').map(row => row.split(','));
+            const headers = rows[0];
+            const data = rows.slice(1).map(row => {
+                return row.reduce((acc, value, index) => {
+                    acc[headers[index].trim()] = value.trim();
+                    return acc;
+                }, {});
+            });
+
+      
+            const user = data.find(item => item.user === username && item.password === password && item.Status === "LIBERADO");
 
             if (user) {
                 localStorage.setItem("logon", 1);
                 localStorage.setItem("usuario", user.user);
                 window.location.replace(user.tela);
             } else {
-                message.innerText = "Usuário ou senha incorretos!";
+                message.innerText = "Usuário ou senha incorreto!";
                 message.style.color = "red";
             }
         })
@@ -37,10 +108,8 @@ document.getElementById('btn_entrar').addEventListener('click', function (event)
         });
 });
 
-
 function Logado() {
     document.addEventListener("click", function (event) {
-    
         if (event.target && event.target.classList.contains("exit")) {
             localStorage.clear();
             localStorage.setItem("logon", 0);
@@ -48,7 +117,6 @@ function Logado() {
         }
     });
 }
-
 
 window.onload = function () {
     Logado();
